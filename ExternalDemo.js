550d1988e30b9eb69500af1fd0af9aa9838725ca
Nilesh Chauhan <nileshprogramming@gmail.com>
2019-07-28 02:24:15 +0530

    TEMP CHANGE 1

--- ExternalDemo.js
+++ ExternalDemo.js
@@ -2,35 +2,15 @@
 "use strict";
 
 function myFunction() {
-
-    let person = {
-        getGreeting() {
-            return "Hello from person";
-        }
-    };//
-    
-    let dog = {
-        getGreeting() {
-            return "Hello from dog";
-        }
-    };
-    
-    
-    let friend = {
-        getGreeting() {
-            return Object.getPrototypeOf(this).getGreeting.call(this) + ", hi!";
-        }
-    };
-    
-    // set prototype to person
-    Object.setPrototypeOf(friend, person);
-    console.log(friend.getGreeting());                      // "Hello, hi!"
-    console.log(Object.getPrototypeOf(friend) === person);  // true
-    console.log(Object.getPrototypeOf(friend).getGreeting());
-    
-    // set prototype to dog
-    Object.setPrototypeOf(friend, dog);
-    console.log(friend.getGreeting());                      // "Woof, hi!"
-    console.log(Object.getPrototypeOf(friend) === dog);     // true
-
+  function UiComponent() {
+    var _this = this; // (A)
+    var button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+      console.log("CLICK");
+      _this.handleClick(); // (B)
+    }); //THIS IS TEMP CHANGE.
+  }
+  UiComponent.prototype.handleClick = function() {
+    //    ···
+  };
 }
